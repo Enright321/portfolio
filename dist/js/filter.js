4d720476d65293c53aa385c40c0465be4f1a0776
Enright321 <enrightjoe4@gmail.com>
2021-08-27 06:24:27 -0500

    MERN Ecommerce Site

--- dist/js/filter.js
+++ dist/js/filter.js
@@ -9,6 +9,7 @@ let item_f = document.querySelector('.item-f');
 let item_g = document.querySelector('.item-g');
 let item_h = document.querySelector('.item-h');
 let item_i = document.querySelector('.item-i');
+let item_aa = document.querySelector('.item-aa');
 
 filter.addEventListener('keyup', (e) => {
   const text = e.target.value.toLowerCase();
@@ -25,11 +26,13 @@ filter.addEventListener('keyup', (e) => {
     item_g.style.display = 'none';
     item_h.style.display = 'none';
     item_i.style.display = 'none';
+    item_aa.style.display = 'none';
   } else if (text === 'bootstrap') {
     item_c.style.display = 'none';
     item_e.style.display = 'none';
     item_f.style.display = 'none';
     item_i.style.display = 'none';
+    item_aa.style.display = 'none';
   } else if (text === 'django') {
     item_a.style.display = 'none';
     item_b.style.display = 'none';
@@ -39,6 +42,7 @@ filter.addEventListener('keyup', (e) => {
     item_g.style.display = 'none';
     item_h.style.display = 'none';
     item_i.style.display = 'none';
+    item_aa.style.display = 'none';
   } else if (text === 'html/css') {
     item_b.style.display = 'none';
     item_f.style.display = 'none';
@@ -53,6 +57,7 @@ filter.addEventListener('keyup', (e) => {
     item_f.style.display = 'none';
     item_h.style.display = 'none';
     item_i.style.display = 'none';
+    item_aa.style.display = 'none';
   } else if (text === 'api') {
     item_b.style.display = 'none';
     item_c.style.display = 'none';
@@ -68,5 +73,6 @@ filter.addEventListener('keyup', (e) => {
     item_g.style.display = 'initial';
     item_h.style.display = 'initial';
     item_i.style.display = 'initial';
+    item_aa.style.display = 'none';
   }
 });
