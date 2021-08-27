5b3f709dadc8c0580d7ad933db1fce7c080cb36c
Enright321 <enrightjoe4@gmail.com>
2021-08-27 06:48:28 -0500

    fix

--- dist/js/filter.js
+++ dist/js/filter.js
@@ -9,30 +9,28 @@ let item_f = document.querySelector('.item-f');
 let item_g = document.querySelector('.item-g');
 let item_h = document.querySelector('.item-h');
 let item_i = document.querySelector('.item-i');
-let item_aa = document.querySelector('.item-aa');
 
 filter.addEventListener('keyup', (e) => {
   const text = e.target.value.toLowerCase();
 
   if (text == 'javascript') {
-    item_b.style.display = 'none';
     item_c.style.display = 'none';
     item_g.style.display = 'none';
   } else if (text === 'python') {
     item_a.style.display = 'none';
+    item_b.style.display = 'none';
     item_d.style.display = 'none';
     item_e.style.display = 'none';
     item_f.style.display = 'none';
     item_g.style.display = 'none';
     item_h.style.display = 'none';
     item_i.style.display = 'none';
-    item_aa.style.display = 'none';
   } else if (text === 'bootstrap') {
+    item_b.style.display = 'none';
     item_c.style.display = 'none';
     item_e.style.display = 'none';
     item_f.style.display = 'none';
     item_i.style.display = 'none';
-    item_aa.style.display = 'none';
   } else if (text === 'django') {
     item_a.style.display = 'none';
     item_b.style.display = 'none';
@@ -42,9 +40,7 @@ filter.addEventListener('keyup', (e) => {
     item_g.style.display = 'none';
     item_h.style.display = 'none';
     item_i.style.display = 'none';
-    item_aa.style.display = 'none';
   } else if (text === 'html/css') {
-    item_b.style.display = 'none';
     item_f.style.display = 'none';
     item_g.style.display = 'none';
     item_h.style.display = 'none';
@@ -57,9 +53,7 @@ filter.addEventListener('keyup', (e) => {
     item_f.style.display = 'none';
     item_h.style.display = 'none';
     item_i.style.display = 'none';
-    item_aa.style.display = 'none';
   } else if (text === 'api') {
-    item_b.style.display = 'none';
     item_c.style.display = 'none';
     item_g.style.display = 'none';
     item_i.style.display = 'none';
@@ -73,6 +67,5 @@ filter.addEventListener('keyup', (e) => {
     item_g.style.display = 'initial';
     item_h.style.display = 'initial';
     item_i.style.display = 'initial';
-    item_aa.style.display = 'initial';
   }
 });
